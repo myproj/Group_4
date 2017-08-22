aa6e7490abb42ba3446e6f1b24ce3fd6315439d2
Sagar Gupta <sagarguptasml@gmail.com>
2017-08-22 16:38:21 +0530

    Handle Negative numbers.
    Ask for input of a positive number.

--- factorial.cpp
+++ factorial.cpp
@@ -1,6 +1,13 @@
 #include "functions.h"
-
+#include <iostream>
+using namespace std;
 int factorial(int n){
+	if(n<0)
+	{
+		cout<<"Enter a positive number\n";
+		cin>>n;
+		factorial(n);
+	}
     if(n!=1){
 	return(n * factorial(n-1));
     }
